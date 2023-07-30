758fb08959d1fed9c3e33b81cf0e64066409c3f4
olhaizbash <olgaizbash@gmail.com>
2023-07-30 15:11:47 +0300

    second init

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryItemRefs = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function createGalleryItemElements(elements) {
+    return elements.map((element) => `<li class="gallery__item">
+   <a class="gallery__link" href="${element.original}">
+      <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+   </a>
+</li>`).join('');
+}
+
+galleryItemRefs.innerHTML = createGalleryItemElements(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: 'true',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: '250',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
 
+import throttle from "lodash.throttle";
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+} 
+const KEY_LOCAL = "feedback-form-state";
+const formData = {};
+saveInput();
+refs.form.addEventListener('input', throttle((onInput), 500));
+refs.form.addEventListener('submit', onSubmit);
+function onInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(KEY_LOCAL, JSON.stringify(formData));
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(KEY_LOCAL)));
+    event.currentTarget.reset();
+    localStorage.removeItem(KEY_LOCAL);
+    
+}
+
+function saveInput() {
+    const saveInfo = localStorage.getItem(KEY_LOCAL);
+    if (saveInfo) {
+        const data = JSON.parse(saveInfo);
+        refs.input.value = data.email;
+        refs.textarea.value = data.message;
+    }
+}
